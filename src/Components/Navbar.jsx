012c69db3bb1e39e190a6bbf7f6371701011fee5
Chamod Perera <chamod914@gmail.com>
2024-01-07 23:25:07 +0530

    Update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,10 +2,24 @@ import React from 'react'
 import { useState } from 'react'
 
 import {logo, arrow_down, arrow_up, todo, calender, reminders, planning } from '../images/index'
+import Dropdown from './Dropdown'
 
 const Navbar = () => {
     const [navToggleF, setNavtoggleF] = useState(false)
     const [navToggleC, setNavtoggleC] = useState(false)
+
+    const featuresList = [
+        {lable: "Todo List" , link: '#', image: todo},
+        {lable:"Calender" , link: '#' , image: calender},
+        {lable:"Reminders" , link: '#' , image: reminders},
+        {lable:"Planning" , link: '#' , image: planning}
+    ]
+
+    const companyList = [
+        {lable: "History" , link: '#'},
+        {lable: "Our Team" , link: '#'},
+        {lable: "Blog" , link: '#'}
+    ]
 
     return (
         //Navbar Main
@@ -22,7 +36,7 @@ const Navbar = () => {
                     <ul className='flex justify-start items-start cursor-pointer '>
 
                         {/* Features */}
-                        <li className='mr-[20px]' onClick={() => {setNavtoggleF ((prev) => !prev); ((prev) => !prev)}}>
+                        <li className='mr-[20px]' onClick={() => setNavtoggleF ((prev) => !prev)}>
                             <div className=' flex justify-center items-center hover:text-ABlack hover:font-bold'>
                                 <a href='#'>Features</a>
                                 <img    src={navToggleF ? arrow_up : arrow_down} 
@@ -30,52 +44,20 @@ const Navbar = () => {
                                 />
                             </div>
 
-                            {/* Features hidden compartment */}
-                            <div className={`${navToggleF ? 'absolute' : 'hidden'} py-[15px] px-[25px] leading-[2rem] left-[90px] top-[40px] shadow-2xl rounded-md outline-none border-none`}>
-    
-                                {/* Features-Todo */}
-                                <div className='flex justify-start items-center'>
-                                    <img src={todo} className='w-[15px] h-[15px] mr-[10px]' />
-                                    <p className='hover:text-ABlack hover:font-bold'>
-                                        <a href='#'>Todo List</a>
-                                    </p>
-                                </div>
+                            <Dropdown isOpen={navToggleF} items={featuresList} compartment="F"/>
 
-                                {/* Features-Calender */}
-                                <div className='flex justify-start items-center'>
-                                    <img src={calender} className='w-[15px] h-[15px] mr-[10px]'/>
-                                    <p className='hover:text-ABlack hover:font-bold'><a href='#'>Calender</a></p>
-                                </div>
-
-                                {/* Features-Reminders */}
-                                <div className='flex justify-start items-center'>
-                                    <img src={reminders} className='w-[15px] h-[15px] mr-[10px]' />
-                                    <p className='hover:text-ABlack hover:font-bold'><a href='#'>Reminders</a></p>
-                                </div>
-
-                                {/* Features-Planning */}
-                                <div className='flex justify-start items-center'>
-                                    <img src={planning} className='w-[15px] h-[15px] mr-[10px]' />
-                                    <p className='hover:text-ABlack hover:font-bold'><a href='#'>Planning</a></p>
-                                </div>
-                            </div>
                         </li>
 
                         {/* Company */}
-                        <li className='mr-[20px] flex justify-center items-center ' onClick={() => { setNavtoggleC ((prev) => !prev) ;  ((prev) => !prev)}}>
+                        <li className='mr-[20px] flex justify-center items-center ' onClick={() => setNavtoggleC ((prev) => !prev)}>
                             <div className=' flex justify-center items-center hover:text-ABlack hover:font-bold'>
                                 <p><a href='#'>Company</a></p>
                                 <img    src={navToggleC ? arrow_up : arrow_down} 
                                         className='w-[10px] h-[10px] ml-[5px]'
                                 />
                             </div>
-                        
-                            {/* Company hidden compartment */}
-                            <div className={`${navToggleC ? 'absolute' : 'hidden'} py-[15px] px-[25px] leading-[2rem] left-[290px] top-[40px] shadow-2xl rounded-md outline-none border-none`}>
-                                <p className='hover:text-ABlack hover:font-bold'><a href='#'>Histrory</a></p>
-                                <p className='hover:text-ABlack hover:font-bold'><a href='#'>Our Team</a></p>
-                                <p className='hover:text-ABlack hover:font-bold'><a href='#'>Blog</a></p>
-                            </div>
+
+                            <Dropdown isOpen={navToggleC} items={companyList} />
                         </li>
 
                         {/* Careers */}
