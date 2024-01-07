985f141c2efd36f8e484e08ea540a851c20767e7
Chamod Perera <chamod914@gmail.com>
2024-01-07 15:59:47 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,47 +1,94 @@
 import React from 'react'
 import { useState } from 'react'
 
-import {logo, arrow_down, arrow_up } from '../images/index'
+import {logo, arrow_down, arrow_up, todo, calender, reminders, planning } from '../images/index'
 
 const Navbar = () => {
     const [navToggleF, setNavtoggleF] = useState(false)
     const [navToggleC, setNavtoggleC] = useState(false)
+    const [hiddenMenuF, sethiddenMenuF] = useState(false)
 
     return (
-        <div className='w-full flex justify-between items-center text-MGray text-[0.8rem]'>
+        //Navbar Main
+        <div className='w-full flex justify-between text-center items-start text-MGray text-[0.8rem]'>
+
+            {/* Logo Section */}
             <div className='flex justify-start items-start flex-1'>
                 <div className='w-[150px]'>
                     <img src={logo} />
                 </div>
 
+            {/* Nav links */}
                 <div>
-                    <ul className='flex justify-start items-center cursor-pointer '>
-                        <li className='mr-[20px] flex justify-center items-center hover:text-ABlack hover:font-bold'>
-                            <a href='#'>Features</a>
-                            <img    src={navToggleF ? arrow_up : arrow_down} 
-                                    className='w-[10px] h-[10px] ml-[5px]'
-                                    onClick={() => setNavtoggleF ((prev) => !prev)}/>
+                    <ul className='flex justify-start items-start cursor-pointer '>
+
+                        {/* Features */}
+                        <li className='mr-[20px]' onClick={() => {setNavtoggleF ((prev) => !prev);sethiddenMenuF ((prev) => !prev)}}>
+                            <div className=' flex justify-center items-center hover:text-ABlack hover:font-bold'>
+                                <a href='#'>Features</a>
+                                <img    src={navToggleF ? arrow_up : arrow_down} 
+                                        className='w-[10px] h-[10px] ml-[5px]'
+                                />
+                            </div>
+
+                            {/* Features hidden compartment */}
+                            <div className={`${hiddenMenuF ? 'block' : 'hidden'} border-[1px] border-MGray py-[15px] px-[25px] leading-[2rem] `}>
+        
+                                {/* Features-Todo */}
+                                <div className='flex justify-start items-center'>
+                                    <img src={todo} className='w-[15px] h-[15px] mr-[10px]' />
+                                    <p>
+                                        <a href='#'>Todo List</a>
+                                    </p>
+                                </div>
+
+                                {/* Features-Calender */}
+                                <div className='flex justify-start items-center'>
+                                    <img src={calender} className='w-[15px] h-[15px] mr-[10px]'/>
+                                    <p><a href='#'>Calender</a></p>
+                                </div>
+
+                                {/* Features-Reminders */}
+                                <div className='flex justify-start items-center'>
+                                    <img src={reminders} className='w-[15px] h-[15px] mr-[10px]' />
+                                    <p><a href='#'>Reminders</a></p>
+                                </div>
+
+                                {/* Features-Planning */}
+                                <div className='flex justify-start items-center'>
+                                    <img src={planning} className='w-[15px] h-[15px] mr-[10px]' />
+                                    <p><a href='#'>Planning</a></p>
+                                </div>
+                            </div>
                         </li>
 
-                        <li className='mr-[20px] flex justify-center items-center hover:text-ABlack hover:font-bold'>
+                        {/* Company */}
+                        <li className='mr-[20px] flex justify-center items-center hover:text-ABlack hover:font-bold' onClick={() => setNavtoggleC ((prev) => !prev)}>
                             <a href='#'>Company</a>
                             <img    src={navToggleC ? arrow_up : arrow_down} 
                                     className='w-[10px] h-[10px] ml-[5px]'
-                                    onClick={() => setNavtoggleC ((prev) => !prev)}/>
+                                    />
                         </li>
 
+                        {/* Careers */}
                         <li className='mr-[20px] hover:text-ABlack hover:font-bold'><a href='#'>Careers</a></li>
+
+                        {/* About */}
                         <li className='mr-[20px] hover:text-ABlack hover:font-bold'><a href='#'>About</a></li>
                     </ul>
                 </div>
             </div>
             
 
+            {/* Button Section */}
             <div className='flex justify-between items-center'>
+
+                {/* Login Link */}
                 <div className='mr-[25px] hover:text-ABlack hover:font-bold' >
                     <a href='#'>Login</a>
                 </div>
 
+                {/* Register Link */}
                 <div className='border-[1px] border-MGray rounded-[12px] px-[10px] py-[5px] hover:text-ABlack hover:border-ABlack hover:font-bold'>
                     <a href='#'>Register</a>
                 </div>
